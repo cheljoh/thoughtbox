768095fc65dbd381a2a1a2954eb3ba4df5814ec0
Chelsea Johnson <cheljoh@gmail.com>
2016-06-06 12:04:27 -0600

    validating links

--- spec/features/user_submits_link_spec.rb
+++ spec/features/user_submits_link_spec.rb
@@ -11,11 +11,27 @@ RSpec.feature "UserSubmitsLink", type: feature do
     fill_in "Password", with: "hello"
     click_button "Login!"
 
-    fill_in "Url", with: "google.com"
+    fill_in "Url", with: "http://google.com"
     fill_in "Title", with: "best site ever"
     click_button "Submit"
 
-    expect(page).to have_content("google.com")
+    expect(page).to have_content("http://google.com")
     expect(page).to have_content("best site ever")
+  end
+
+  scenario "user submits invalid link" do
+    user = User.create(email: "hi@example.com", password: "hello", password_confirmation: "hello")
+
+    visit "/"
+
+    fill_in "Email", with: "hi@example.com"
+    fill_in "Password", with: "hello"
+    click_button "Login!"
+
+    fill_in "Url", with: "hahahaha"
+    fill_in "Title", with: "best site ever"
+    click_button "Submit"
+
+    expect(page).to have_content("Your link is not valid, please try again")
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates urls" do
+    link1 = Link.create(title: "hey", url: "http://google.com")
+    expect(link1.valid?).to be true
+    link2 = Link.create(title: "hey", url: "poop")
+    expect(link2.valid?).to be false
+  end
 end
