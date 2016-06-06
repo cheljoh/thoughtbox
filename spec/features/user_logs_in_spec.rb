c83ca91db01c09a594d872570f6bd4ef1fd7efa6
Chelsea Johnson <cheljoh@gmail.com>
2016-06-06 10:24:01 -0600

    validate password and password confirmation

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -32,4 +32,24 @@ RSpec.feature "UserLogsIn", type: feature do
 
     expect(page).to have_content("Please enter a valid username and password")
   end
+
+  scenario "password and password confirmation must match" do
+    visit "/users/new"
+
+    fill_in "Email", with: "chelsea@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "hello"
+    click_button "Create Account"
+
+    expect(page).to have_content("Please enter a valid username and password")
+
+    visit "/users/new"
+
+    fill_in "Email", with: "chelsea@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Create Account"
+
+    expect(page).to have_content("Check out these kewwwll linkkss")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe User, type: :model do
     user2 = User.create(email: "hello@cool.com", password: "hello", password_confirmation: "hello")
     expect(user2.valid?).to be false
   end
+
+  it "confirms password and password confirmation" do
+    user1 = User.create(email: "hello@cool.com", password: "hello", password_confirmation: "yo")
+    expect(user1.valid?).to be false
+    user2 = User.create(email: "hello@cool.com", password: "hello", password_confirmation: "hello")
+    expect(user2.valid?).to be true
+  end
 end
