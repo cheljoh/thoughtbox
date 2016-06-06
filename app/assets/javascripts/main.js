7baea443dfeb66c859acab973f1809f3ed24e4aa
Chelsea Johnson <cheljoh@gmail.com>
2016-06-06 16:50:22 -0600

    fixed failing test

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,12 +12,12 @@ function getLinks(){
   $.getJSON('api/v1/links', function(links){
     $.each(links, function(index, link){
       if (link.read){
-        buttonReadContent = "<button id=read-" + link.id + " class='unread-link btn'> Mark as Unread </button>";
+        buttonReadContent = "<button id=read-" + link.id + " class='unread-link'> Mark as Unread </button>";
         linkStyle = "color:#0000FF";
         readOrUnread = "Already perused it";
       }
       else {
-        buttonReadContent = "<button id=read-" + link.id + " class='read-link btn'> Mark as Read </button>";
+        buttonReadContent = "<button id=read-" + link.id + " class='read-link'> Mark as Read </button>";
         linkStyle = "color:#FF0000";
         readOrUnread = "Need to check it out";
       }
