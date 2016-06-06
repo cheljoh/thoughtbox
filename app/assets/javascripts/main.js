a4a055770f3eb7ae0ba41525a04945f92cdc1bc7
Chelsea Johnson <cheljoh@gmail.com>
2016-06-06 14:47:19 -0600

    trying to have buttons on rails side and change dynamically using jS

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,29 +1,29 @@
 $(document).ready(function(){
-  getLinks();
+  // getLinks();
   $("body").on("click", "button.read-link", unreadToRead);
   $("body").on("click", "button.unread-link", readToUnread);
 });
 
-function getLinks(){
-  var buttonReadContent = "";
-  $.getJSON('api/v1/links', function(links){
-    $.each(links, function(index, link){
-      if (link.read){ //read is true
-        buttonReadContent = "<button id=read-" + link.id + " class='unread-link'> Mark as Unread </button>";
-        linkStyle = "color:#0000FF";
-      }
-      else { //read is false
-        buttonReadContent = "<button id=read-" + link.id + " class='read-link'> Mark as Read </button>";
-        linkStyle = "color:#FF0000";
-      }
-      $(".all-links").append(
-        "<p>Title: " + link.title + "</p><br>" +
-        "<p style=" + linkStyle + " id=url-" + link.id + ">Url: " + link.url + "</p><br>" +
-        buttonReadContent + "<br><br>"
-      );
-    });
-  });
-}
+// function getLinks(){
+//   var buttonReadContent = "";
+//   $.getJSON('api/v1/links', function(links){
+//     $.each(links, function(index, link){
+//       if (link.read){ //read is true
+//         buttonReadContent = "<button id=read-" + link.id + " class='unread-link'> Mark as Unread </button>";
+//         linkStyle = "color:#0000FF";
+//       }
+//       else { //read is false
+//         buttonReadContent = "<button id=read-" + link.id + " class='read-link'> Mark as Read </button>";
+//         linkStyle = "color:#FF0000";
+//       }
+//       $(".all-links").append(
+//         "<p>Title: " + link.title + "</p><br>" +
+//         "<p style=" + linkStyle + " id=url-" + link.id + ">Url: " + link.url + "</p><br>" +
+//         buttonReadContent + "<br><br>"
+//       );
+//     });
+//   });
+// }
 
 function unreadToRead(){
   var id = getId($(this).attr("id"));
@@ -34,8 +34,10 @@ function unreadToRead(){
     dataType: "json",
     data: link,
     success: function(){
-      $(".all-links").empty();
-      getLinks();
+      // $(".all-links").empty();
+      // getLinks();
+      $("#read-" + id).text("Mark AS UNREAD");
+      $("#url-" + id).css("color", "red");
     },
     error: function(){
       console.log("Something went wrong");
@@ -52,8 +54,10 @@ function readToUnread(){
     dataType: "json",
     data: link,
     success: function(){
-      $(".all-links").empty();
-      getLinks();
+      // $(".all-links").empty();
+      // getLinks();
+      $("#read-" + id).text("Mark AS READ");
+      $("#url-" + id).css("color", "blue");
     },
     error: function(){
       console.log("Something went wrong");
